38f0b0c0540fdb28158db7e60c2d74d57985c446
ovejabu <eliabudelli@gmail.com>
2024-06-27 13:10:47 -0300

    Update host and port configuration for uvicorn

--- challenge/api.py
+++ challenge/api.py
@@ -116,4 +116,4 @@ async def post_predict(request: PredictionRequest) -> PredictionResponse:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
